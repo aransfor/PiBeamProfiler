3f4ac2e24583abcbcf2262c5dab97133d76b3386
Maxx Tepper <maxxtepper@gmail.com>
2015-09-22 10:48:42 -0700

    Documentation added

--- MT_g2d.py
+++ MT_g2d.py
@@ -14,28 +14,28 @@ class Gaussian2D(object):
     2D Gaussian class
     """
     def __init__(self, data):
-	"""Values generated as result of fit"""
+	#Values generated as result of fit
 	self._x0 = None  # center column
         self._y0 = None  # center row
         self._w_a = None  # long axis length in pixels
         self._w_b = None  # short axis length in pixels
         self._rho = None  # amplitude of 2D Gaussian
 	
-	"""Initial guesses required for the fitting"""
+	#Initial guesses required for the fitting
         self._x0_g = 50  # center column
         self._y0_g = 30  # center row
         self._w_a_g = 20  # long axis length in pixels
         self._w_b_g = 9  # short axis length in pixels
         self._rho_g = 7  # amplitude of 2D Gaussian
 
-	"""Error produced required for the fitting"""
+	#Error produced required for the fitting
         self._x0_err = None
         self._y0_err = None
         self._w_a_err = None
         self._w_b_err = None
         self._rho_err = None
 
-	"""Values for the user as a result of the fitting"""
+	#Values for the user as a result of the fitting
 	self._x0_pos = None
 	self._y0_pos = None
 	self._w_a_len = None
@@ -43,11 +43,13 @@ class Gaussian2D(object):
 	self._w_a_pix = None
 	self._w_b_pix = None
 
-	"""Values based on resolution of image"""
+	#Values based on resolution of image
 	self._pixel_size = 0.0000014
 	self._a_pixel_len = 2592
 	self._b_pixel_len = 1944
 	self._pixel_res = 100
+	self._a_pixel_factor = self._a_pixel_len / self._pixel_res	#Factors for rescaling the Gauss widths according
+	self._b_pixel_factor = self._b_pixel_len / self._pixel_res	#to the pixel size and picture resolution
 
         if data is None:
             print "Please load data."
@@ -58,19 +60,19 @@ class Gaussian2D(object):
     def _fit_2DGaussian(self):
         initial_guess = (self._rho_g, self._x0_g, self._y0_g, self._w_a_g, self._w_b_g)
 
-        """Get the row & column numbers"""
+        #Get the row & column numbers
         row_num = len(self._data)
         col_num = len(self._data[0])
 
-        """Create x and y indices"""
+        #Create x and y indices
         x = _n.linspace(0, col_num-1, col_num)
         y = _n.linspace(0, row_num-1, row_num)
         x, y = _n.meshgrid(x, y)
 
-        """The fitting"""
+        #The fitting
         popt, pcov = _curve_fit(self._twoD_Gaussian, (x, y),
                                     self._data.flatten(), p0=initial_guess)
-        """Return the fitting parameter results, and convert the positions and lengths to millimeters"""
+        #Return the fitting parameter results, and convert the positions and lengths to millimeters
         self._rho = popt[0]
         self._x0 = popt[1]
         self._y0 = popt[2]
@@ -82,10 +84,10 @@ class Gaussian2D(object):
         self._w_a_err = _n.sqrt(pcov[3][3])
         self._w_b_err = _n.sqrt(pcov[4][4])
 
-	self._w_a_pix = 25.92 * self._w_a
-	self._w_b_pix = 19.44 * self._w_b 
-	self._w_a_len = 25.92 * self._pixel_size * self._w_a * 1000
-	self._w_b_len = 19.44 * self._pixel_size * self._w_b * 1000
+	self._w_a_pix = self._a_pixel_factor * self._w_a		#Scales the Gauss widths in terms of pixels
+	self._w_b_pix = self._b_pixel_factor * self._w_b 
+	self._w_a_len = self._a_pixel_factor * self._pixel_size * self._w_a * 1000	#Scales the Gauss widths in terms of mm
+	self._w_b_len = self._b_pixel_factor * self._pixel_size * self._w_b * 1000
 
     def _twoD_Gaussian(self, (x, y), rho, x0, y0, w_a, w_b):
         """
